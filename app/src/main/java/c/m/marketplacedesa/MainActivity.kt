63493e1e53c133ac74f1fbb1e3983232f3e73dbb
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-08 23:09:06 +0700

    add main activity layout

--- app/src/main/java/c/m/marketplacedesa/MainActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/MainActivity.kt
@@ -1,12 +1,33 @@
 package c.m.marketplacedesa
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar_main)
+        supportActionBar?.apply { title = getString(R.string.app_name) }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_cart -> true
+            R.id.menu_user -> true
+            R.id.menu_setting -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
