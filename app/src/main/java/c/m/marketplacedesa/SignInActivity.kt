6fc3f501a2b3c3ac20a7650b76773b12ee6c46da
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-09 14:39:06 +0700

    add sign in activity layout

---
+++ app/src/main/java/c/m/marketplacedesa/SignInActivity.kt
@@ -0,0 +1,31 @@
+package c.m.marketplacedesa
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        setSupportActionBar(toolbar_sign_in)
+        supportActionBar?.apply { title = "" }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_sign_in, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_setting -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
