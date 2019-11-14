8fb59ad5060ebe063ce4c8f2be2a94920e53eeb1
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-15 00:41:09 +0700

    edit layout user profile

--- app/src/main/java/c/m/marketplacedesa/ui/profile/ProfileActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/ui/profile/ProfileActivity.kt
@@ -1,13 +1,37 @@
 package c.m.marketplacedesa.ui.profile
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import c.m.marketplacedesa.R
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        setSupportActionBar(toolbar_profile)
+        supportActionBar?.apply {
+            title = ""
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_edit_profile -> true
+            R.id.menu_sign_out -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
