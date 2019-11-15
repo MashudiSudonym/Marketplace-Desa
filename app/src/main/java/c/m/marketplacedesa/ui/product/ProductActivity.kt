503b3e8ede6090daedc86a4e39b7e1ecaac67b10
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-16 00:38:46 +0700

    rename product activity to store activity and rename details activity to product activity

--- app/src/main/java/c/m/marketplacedesa/ui/product/ProductActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/ui/product/ProductActivity.kt
@@ -3,18 +3,11 @@ package c.m.marketplacedesa.ui.product
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import c.m.marketplacedesa.R
-import kotlinx.android.synthetic.main.activity_product.*
 
 class ProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
-        setSupportActionBar(toolbar_product)
-        supportActionBar?.apply {
-            title = ""
-            setDisplayHomeAsUpEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
-        }
     }
 }
