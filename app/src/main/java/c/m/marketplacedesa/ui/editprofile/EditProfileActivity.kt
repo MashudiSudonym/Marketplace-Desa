73df7c202578c7ae6422181fb6f352abd3834edd
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-15 23:17:36 +0700

    add edit profile activity

---
+++ app/src/main/java/c/m/marketplacedesa/ui/editprofile/EditProfileActivity.kt
@@ -0,0 +1,13 @@
+package c.m.marketplacedesa.ui.editprofile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import c.m.marketplacedesa.R
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
