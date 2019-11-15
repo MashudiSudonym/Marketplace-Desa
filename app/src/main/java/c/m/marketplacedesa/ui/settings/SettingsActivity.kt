2832e13156608e026f6d1a445f42f3a749c82ed8
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-15 22:41:33 +0700

    finish settings activity, settings layout

--- app/src/main/java/c/m/marketplacedesa/ui/settings/SettingsActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/ui/settings/SettingsActivity.kt
@@ -3,16 +3,23 @@ package c.m.marketplacedesa.ui.settings
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import c.m.marketplacedesa.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
             .commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setSupportActionBar(toolbar_setting)
+        supportActionBar?.apply {
+            title = getString(R.string.title_activity_settings)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
 }
