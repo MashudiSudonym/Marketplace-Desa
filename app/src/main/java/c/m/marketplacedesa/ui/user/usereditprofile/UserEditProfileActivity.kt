389169067a44dd6f24c06848ff80323a466f90c0
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-11-20 20:05:29 +0700

    add user edit profile layout

--- app/src/main/java/c/m/marketplacedesa/ui/user/usereditprofile/UserEditProfileActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/ui/user/usereditprofile/UserEditProfileActivity.kt
@@ -1,13 +1,37 @@
 package c.m.marketplacedesa.ui.user.usereditprofile
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import c.m.marketplacedesa.R
+import kotlinx.android.synthetic.main.activity_user_edit_profile.*
 
 class UserEditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_edit_profile)
+        setSupportActionBar(toolbar_user_edit_profile)
+        supportActionBar?.apply {
+            title = getString(R.string.edit_profile)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    // app bar menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_user_edit_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_save -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
