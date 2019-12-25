28f348280e3da494b8fdccd80046d2e690f214c8
masrobot <muhamadmashudiardiwinata@gmail.com>
2019-12-25 13:06:52 +0700

    add permission

--- app/src/main/java/c/m/marketplacedesa/ui/signin/SignInActivity.kt
+++ app/src/main/java/c/m/marketplacedesa/ui/signin/SignInActivity.kt
@@ -1,5 +1,6 @@
 package c.m.marketplacedesa.ui.signin
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -15,6 +16,7 @@ import c.m.marketplacedesa.util.Constants
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
+import com.github.babedev.dexter.dsl.runtimePermission
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
@@ -27,6 +29,19 @@ class SignInActivity : AppCompatActivity() {
         setSupportActionBar(toolbar_sign_in)
         supportActionBar?.apply { title = getString(R.string.app_name) }
 
+        // validate location permission
+        runtimePermission {
+            permissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) {
+                checked { }
+            }
+        }
+
+        // sign in button
         btn_sign_in.setOnClickListener {
             startActivityForResult(
                 AuthUI.getInstance()
