6d71d72a1cb196aa8dd42b899a179791863aea0b
masrobot <muhamadmashudiardiwinata@gmail.com>
2020-01-01 21:12:37 +0700

    layout edit store information

---
+++ app/src/main/java/c/m/marketplacedesa/ui/seller/sellereditstoreinformation/SellerEditStoreInformationActivity.kt
@@ -0,0 +1,13 @@
+package c.m.marketplacedesa.ui.seller.sellereditstoreinformation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import c.m.marketplacedesa.R
+
+class SellerEditStoreInformationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_edit_store_information)
+    }
+}
